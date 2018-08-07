ddddc159015180daa74b221914556a9ca727d76b
eyuyitung <eyuyitung@gmail.com>
2018-08-07 08:52:10 -0400

    added credential handling, updated gitignore and cleaned up project structure

--- src/list_metrics.py
+++ src/list_metrics.py
@@ -1,9 +1,8 @@
 import SoftLayer
+import config
 
-USERNAME = 'snewton@densify.com'
-API_KEY = ''
 
-client = SoftLayer.create_client_from_env(USERNAME,API_KEY)
+client = SoftLayer.create_client_from_env(config.USERNAME, config.API_KEY)
 mgr = SoftLayer.HardwareManager(client)
 object_mask = "mask[hostname,monitoringRobot[robotStatus]]"
 result = mgr.list_hardware(mask=object_mask)
