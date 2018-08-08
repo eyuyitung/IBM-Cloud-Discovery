a74c4fdf568ddd5f4d02c775522c026b35848b2c
eyuyitung <eyuyitung@gmail.com>
2018-08-08 13:34:00 -0400

    more file structuring, working metric collection on 5 min interval, seperate config and metric structures

--- src/list_metrics.py
+++ src/list_metrics.py
@@ -3,15 +3,21 @@ import config
 import json
 from datetime import *
 import time
+
+
+####
 start = time.perf_counter()
+print("start")
+####
 
 client = SoftLayer.create_client_from_env(config.USERNAME, config.API_KEY)
 accountService = client['SoftLayer_Account']
-
 sampleSizeHours = 24
-metricEndDate = (datetime.now(timezone.utc))
-metricStartDate = metricEndDate + timedelta(hours=-sampleSizeHours)
-
+now = (datetime.now(timezone(-timedelta(hours=5))))
+metricEndDate = datetime.strftime(now, "%Y-%m-%dT%H:%M%z")
+metricStartDate = datetime.strftime(now - timedelta(hours=sampleSizeHours), "%Y-%m-%dT%H:%M%z")
+sys_conf = {}
+sys_metrics = {}
 
 
 def print_json(file):
@@ -19,25 +25,32 @@ def print_json(file):
 
 
 def main():
-    systems = {}
-    
-    for virtualGuestConfig in accountService.getVirtualGuests():
-        virtual_id = virtualGuestConfig['id']
-        # print_json(virtualGuestConfig)
-        virtualGuestConfig['operatingSystem'] = client['SoftLayer_Virtual_Guest'].getOperatingSystem(id=virtual_id)
-        virtualGuestConfig['location'] = client['SoftLayer_Virtual_Guest'].getRegionalGroup(id=virtual_id)
-        virtualGuestConfig['location']["datacenter"] = client['SoftLayer_Virtual_Guest'].getDatacenter(id=virtual_id)
-        systems[virtual_id] = [virtualGuestConfig]
-        # instance metrics
-        cpuMetric = client['SoftLayer_Virtual_Guest'].getCpuMetricDataByDate(metricStartDate, metricEndDate, id=virtual_id)
-        cpuMetric = [{'Timestamps': ['2018-08-12:09:15:00Z'], 'Data': ['52.001547']}]  # placeholder values
-        systems[virtual_id].append(cpuMetric)
-
-    print_json(systems)
+    get_config()
 
 
+def get_config():
+    object_mask = "mask[id, maxMemory, maxCpu, datacenter.name, regionalGroup.name, " \
+                  "operatingSystem.softwareLicense.softwareDescription.name]"
+    masked_call = accountService.getVirtualGuests(mask=object_mask)
+    for account in masked_call:
+        sys_conf[account['id']] = [account]
+    get_metrics(sys_conf.keys())
 
 
+def get_metrics(virtual_ids):
+    for virtual_id in virtual_ids:
+        cpu_metric = client.call('Virtual_Guest', 'getCpuMetricDataByDate', metricStartDate, metricEndDate, id=virtual_id)
+        ram_metric = client.call('Virtual_Guest', 'getMemoryMetricDataByDate', metricStartDate, metricEndDate, id=virtual_id)
+        net_metric = client.call('Virtual_Guest', 'getBandwidthDataByDate', metricStartDate, metricEndDate, id=virtual_id)
+        sys_metrics[virtual_id] = [cpu_metric]
+        sys_metrics[virtual_id].append(ram_metric)
+        sys_metrics[virtual_id].append(net_metric)
+
+    print_json(sys_conf)
+    print_json(sys_metrics)
+
+    end = time.perf_counter()
+    print(str(end - start))
 
 
 
@@ -53,6 +66,3 @@ objectMask = "mask[userCount]"
 
 if __name__ == '__main__':
     main()
-
-end = time.perf_counter()
-print(str(end - start))
