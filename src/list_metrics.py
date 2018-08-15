f75a3dcc068725f8386e95ee7efa8ea99f02a708
eyuyitung <eyuyitung@gmail.com>
2018-08-15 12:30:20 -0400

    added more comments, cleaned out disk metrics, more code refactoring

--- src/list_metrics.py
+++ src/list_metrics.py
@@ -9,7 +9,7 @@
         Stephen Newton
 '''
 
-#  Import statements
+#Import statements
 import SoftLayer
 from src import config
 from datetime import *
@@ -35,19 +35,20 @@ now = (datetime.now(timezone(-timedelta(hours=5))))
 metricEndDate = datetime.strftime(now, "%Y-%m-%dT%H:%M%z")
 metricStartDate = datetime.strftime(now - timedelta(hours=sampleSizeHours), "%Y-%m-%dT%H:%M%z")
 
-# The main
+# main entry point
 def main():
     get_config()
 
 # Gets the configuration values for a VSI (CPU, RAM, OS, Name, DataCenter, PowerState)
 def get_config():
 
-    # Creates an array for the system config
+    # Creates a dictionary for the system config
     sys_conf = {}
 
     # The object mask of the query (to limit and filter data returned)
     object_mask = "mask[id, maxMemory, maxCpu, datacenter.name, regionalGroup.name, " \
-                  "operatingSystem.softwareLicense.softwareDescription.name, hostname, powerState, monitoringAgents]"
+                  "operatingSystem.softwareLicense.softwareDescription.name, hostname, powerState, monitoringAgents," \
+                  "blockDevices[device, id, diskImage[capacity, units]]]"
 
     # Queries the SoftLayer API with mask applied
     masked_call = accountService.getVirtualGuests(mask=object_mask)
@@ -60,7 +61,7 @@ def get_config():
         account['maxMemory'] = int(account['maxMemory']/1024)
         account['powerState'] = account['powerState']['name']
 
-        # For each drive on account, print id and account as seperate tagged columns
+        # For each drive on account, print id and account as separate tagged columns
         drive_tag = 1
         for disk in account['blockDevices']:
             if 'diskImage' in disk.keys():  # assuming only relevant drives are disk drives
@@ -75,19 +76,19 @@ def get_config():
         if len(account['monitoringAgents']) > 0:
             account['monitoringAgents'] = account['monitoringAgents'][0]['id']
         else:
-            account['monitoringAgents'] = 'N/A'
+            account['monitoringAgents'] = 'n/a'
 
-        # Stores the fields using the account keys and then sorts them by their IDs
+        # Pulls the fields from the flattened dictionary keys and adds {vsi_id : [config values]} to dictionary
         fields = account.keys()
         sys_conf[account['id']] = list(account.values())
 
     # Creates a data frame and prints it out to CSV file
     df = DataFrame.from_dict(sys_conf, orient='index', columns=fields)
-    df = df.drop('id', axis=1)
+    df = df.drop('id', axis=1)  # drop redundant column
     df.index.name = 'id'
     df.to_csv("conf.csv")
 
-    # Calls the get_metrics method (uses the data frame from above)
+    # Calls the get_metrics method and passes list of vsi ids
     get_metrics(df.index)
 
 
@@ -95,8 +96,8 @@ def get_config():
 def get_metrics(virtual_ids):
 
     metric_dict = {}
-    metrics = ["cpu_utilization", "memory_usage", "disk_usage", "network_in", "network_out"]
-    inst_metric_names = ('Cpu', 'Memory', 'Disk', 'Bandwidth')
+    metrics = ["cpu_utilization", "memory_usage", "network_in", "network_out"]
+    inst_metric_names = ('Cpu', 'Memory', 'Bandwidth')
 
     # Loops through each VSI using a VSI ID grabbed from the account
     for virtual_id in virtual_ids:
@@ -107,46 +108,53 @@ def get_metrics(virtual_ids):
         # Loops through each available metric for the current VSI
         for metric_name in inst_metric_names:
 
-            # Picks the correct metric to query and stores it
-            if metric_name == 'Cpu' or metric_name == 'Memory':
-                response = client.call('Virtual_Guest', 'get' + metric_name + 'MetricDataByDate',
-                                                          metricStartDate, metricEndDate, id=virtual_id)
-            # Does not work as of 8/13/2018 -SHN
-            elif metric_name == 'Disk':
-                response = accountService.getDiskUsageMetricDataByDate(metricStartDate, metricEndDate, id=virtual_id)
-                #response = accountService.getDiskUsageMetricDataFromMetricTrackingObjectSystemByDate(metricStartDate, metricEndDate, id=virtual_id)
-                #  ^^ throws SoftLayer.exceptions.SoftLayerAPIError: SoftLayerAPIError(SoftLayer_Exception): $metrics collection must contain data.
-            else:
+            #  selects correct api call format
+            if metric_name == 'Bandwidth':
                 response = client.call('Virtual_Guest', 'get' + metric_name + 'DataByDate',
-                                                          metricStartDate, metricEndDate, id=virtual_id)
+                                       metricStartDate, metricEndDate, id=virtual_id)
+            else:
+                response = client.call('Virtual_Guest', 'get' + metric_name + 'MetricDataByDate',
+                                       metricStartDate, metricEndDate, id=virtual_id)
 
-            print(response)
-            # If a usage value was collected from the API then continue
-            if len(response) != 0:
-                inst_dict[response[0]['type']] = {}
+            # Reformats API response and stores in inst_dict
+            filter_data_points(inst_dict, response)
 
-                for data_point in response: # populating inst dict with type : {time : value}
-                    if data_point['type'] not in inst_dict.keys():  # add datatype if missing (2 type return from 1 call)
-                        inst_dict[data_point['type']] = {}
-
-                    inst_dict[data_point['type']][data_point['dateTime']] = data_point['counter']
-
-            else:  # if no value returned from api
-                inst_dict['n/a'] = {'n/a': None}
-
-        # Stores values under the current metric
+        # Converts metric dictionaries to time-series, concatenates horizontally, stores by VSI ID
         for key, value in inst_dict.items():
             inst_dict[key] = Series(data=list(value.values()), index=value.keys())
         inst_ts = concat(inst_dict.values(), axis=1, keys=metrics, sort=True)
         metric_dict[virtual_id] = inst_ts
 
-    # Creates a data frame and prints to CSV file
+    # Concatenates VSI dataframes vertically and exports to csv
     df = concat(metric_dict.values(), keys=virtual_ids, sort=True)
+    df.index.names = ['id', "dateTime"]
     df.to_csv("metric.csv")
 
     # time taken to run program
     end = time.perf_counter()
-    print("End:" , str(end - start))
+    print("End:", str(end - start))
+
+
+# Re-formats response, splits call if returning 2+ metric types, and appends {type : {datetime : data}} to inst_dict
+def filter_data_points(inst_dict, response):
+    # If usage values are returned from the API
+    if len(response) != 0:
+
+        # Create dictionary linking data-type to dict time-series
+        inst_dict[response[0]['type']] = {}
+
+        # populating inst dict with type : {time : value}
+        for data_point in response:
+
+            #  handling of multiple metrics returned in one call
+            if data_point['type'] not in inst_dict.keys():
+                inst_dict[data_point['type']] = {}
+
+            #   populating data-type : dict time-series
+            inst_dict[data_point['type']][data_point['dateTime']] = data_point['counter']
+    else:
+        inst_dict['n/a'] = {'n/a': None}
+
 
 # json output shortcut
 def print_json(file):
@@ -156,3 +164,14 @@ def print_json(file):
 # ensure program is running as a main process
 if __name__ == '__main__':
     main()
+
+
+
+'''
+# Does not work as of 8/13/2018 -SHN
+            elif metric_name == 'Disk':
+                accountService.getDiskUsageMetricDataByDate(metricStartDate, metricEndDate, id=virtual_id)
+                #response = accountService.getDiskUsageMetricDataFromMetricTrackingObjectSystemByDate(metricStartDate, metricEndDate, id=virtual_id)
+                #  ^^ throws SoftLayer.exceptions.SoftLayerAPIError: SoftLayerAPIError(SoftLayer_Exception): $metrics collection must contain data.
+            
+'''
