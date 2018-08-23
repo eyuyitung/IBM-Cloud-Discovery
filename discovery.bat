9a897fe30e790fdb384c8e5ce96ab1fd02790979
eyuyitung <eyuyitung@gmail.com>
2018-08-23 13:38:30 -0400

    main.py : mild restructuring to match gcp import format, updated gitignore, added cut down version of gcp batchscript, imports systems into product, but only minimal config data and no workload. BROKEN

---
+++ discovery.bat
@@ -0,0 +1,79 @@
+@ECHO OFF
+
+rem ***************** Edit these default parameters ********************
+
+rem python directory:
+set PYDIR="C:\Program Files\Python36\python.exe"
+
+
+rem **************** edit nothing beyond this point *********************
+
+echo Copyright (c) 2018 Cirba Inc. D/B/A Densify. All Rights Reserved.
+
+
+set ERROR="f"
+set fpath=%~sdp0
+
+
+IF "%1"=="" (
+	echo one week is 168 hours, month is 720 hours, cannot sample beyond 6 weeks [168 hours]
+	set /p HOURS=Please enter your desired sample size in hours: 
+	set /p acc_tag =Please enter username specified in config.py: 
+	echo __________________  Collecting VSI's from provided credentials ...  __________________________
+	call:discoveryFunc %HOURS%
+	GOTO:EOF
+)
+
+:discoveryFunc
+
+echo Clearing previous config ...
+for /f %%i in ('dir /b %fpath%conf') do 2>NUL del /q %fpath%conf\%%i
+2>NUL del /q %fpath%workload.csv
+2>NUL del /q %fpath%config.csv
+
+set temp=%~1
+set Project=%temp:~0,-5%
+
+ECHO  - Step 1 - GCP Discovery
+%PYDIR% %fpath%src\main.py -t %~1 
+
+IF errorlevel 1 GOTO:END
+
+ECHO  - Step 2 - GCP Config
+move %fpath%config.csv %fpath%conf\config.csv
+move %fpath%attributes.csv %fpath%conf\attributes.csv
+move %fpath%workload.csv %fpath%conf\workload.csv
+IF errorlevel 1 (
+	GOTO:END 
+	set FILES="f"
+)
+ECHO  - Step 3 - Creating Manifest
+call "%CIRBA_HOME%\bin\audit-converter.bat" -m %fpath%conf\ "IBM-%acc_tag%"
+IF errorlevel 1 GOTO:END
+
+ECHO  - Step 4 - Creating Repository
+call "%CIRBA_HOME%\bin\audit-converter.bat" %fpath%conf\ %fpath%repo\
+IF errorlevel 1 GOTO:END
+
+ECHO  - Step 5 - Set systems to deleted
+echo skipping step
+rem call "%CIRBA_HOME%\bin\data-tools.bat" runscriptv1 -f %fpath%attr_update.txt -P PROJECT=%PROJECT%
+IF errorlevel 1 GOTO:END
+
+ECHO  - Step 6 - Loading Repository
+call "%CIRBA_HOME%\bin\load-repository.bat" %fpath%repo\ -o
+IF errorlevel 1 GOTO:END
+
+ECHO  - Step 7 - Move Repos to Hist directory
+xcopy repo\* hist\* /s /i /q /Y
+rmdir /s /q repo\
+mkdir repo
+
+ECHO  - Step 8 - Import Attributes
+call "%CIRBA_HOME%\bin\data-tools.bat" ImportAttributes -f %fpath%conf\
+IF errorlevel 1 GOTO:END
+
+:END
+
+IF errorlevel 1 echo Exiting program ...
+pause
--- src/main.py
+++ src/main.py
@@ -4,25 +4,33 @@ from datetime import *
 import time
 from pandas import *
 import json
+import os
+import argparse
 
 #### performance testing
 start = time.perf_counter()
-print("start")
 calls = 0
 ####
 
-client = SoftLayer.create_client_from_env(config.USERNAME, config.API_KEY)
+parser = argparse.ArgumentParser()
 
-sampleSizeHours = 24
+parser.add_argument('-t', dest='hours', default='24',
+                    help='amount of hours to receive data from')
+args = parser.parse_args()
+sampleSizeHours = int(args.hours)
+
+
+client = SoftLayer.create_client_from_env(config.USERNAME, config.API_KEY)
+project_root = os.path.abspath(os.path.join(__file__, "../.."))
+
+
 dc_utc_offset = -5.00
 now = (datetime.now(timezone(timedelta(hours=dc_utc_offset))))
 now = now - timedelta(minutes=now.minute % 5, seconds=now.second, microseconds=now.microsecond)
 dt_format = "%Y-%m-%dT%H:%M%z"
 endDate = datetime.strftime(now, dt_format)
 startDate = datetime.strftime(now - timedelta(hours=sampleSizeHours), dt_format)
-#endDate = now
-#startDate = now - timedelta(hours=sampleSizeHours)
-
+id_host_map = {}
 # Agent config parameters
 
 agentName = 'Cpu, Disk, and Memory Monitoring Agent'
@@ -30,7 +38,7 @@ reports = {
     # Monitor and graph System CPU Usage
     "Graph System CPU Usage": "TRUE",
     # Creates a graph of your system's memory usage.
-    "Graph Memory Usage": "TRUE",
+    "Graph Memory Usage as Percentage": "TRUE",
     # Create graphs for Disk Usage (in MB).
     "Graph Disk Usage": "TRUE",
 }
@@ -38,60 +46,102 @@ reports = {
 
 def main():
     conf_df = get_config()
-    conf_df.to_csv('conf.csv')
+    conf_df.to_csv(project_root + os.path.sep + 'config.csv')
+    attr_df = get_attributes()
+    attr_df.to_csv(project_root + os.path.sep + 'attributes.csv')
     sys_agents = get_agents()
     sys_datatypes = get_agent_datatypes(sys_agents)
     metrics_df = get_agent_metrics(sys_datatypes)
     metrics_df = get_guest_metrics(metrics_df)
-    metrics_df.to_csv('metrics.csv')
+    metrics_df.to_csv(project_root + os.path.sep + 'workload.csv')
     end = time.perf_counter()
-    print(str(end - start))
-    print(calls)
+    print("took %ss to retrieve configuration info from %s vsi(s) and process %s hours of metrics for %s vsi(s).\n"
+          "monitoring agents were accessible on %s%% of account systems." %
+          ('{:.2f}'.format(end - start), str(len(list(conf_df.index))), sampleSizeHours,
+           str(len(list(metrics_df.index.levels[0]))),
+           '{:.0f}'.format(100*(len(list(metrics_df.index.levels[0]))/len(list(conf_df.index))))))
 
 
 def get_config():
-    object_mask = "mask[id, maxMemory, maxCpu, datacenter.name, regionalGroup.name, " \
-                  "operatingSystem.softwareLicense.softwareDescription.name, hostname, powerState," \
-                  "blockDevices[device, id, diskImage[capacity, units]], " \
-                  "monitoringAgents[id, name, configurationValues]]"
+    print("getting config ...")
+
+    object_mask = "mask[hostname, id, maxMemory, maxCpu, operatingSystem.softwareLicense.softwareDescription.name]"
 
     masked_call = client.call('Account', 'getVirtualGuests', mask=object_mask)
-    sys_conf = {}
-    global calls
-    calls += 1
-    print("get config", calls)
-    for account in masked_call:
-        account['datacenter'] = account['datacenter']['name']
-        account['operatingSystem'] = account['operatingSystem']['softwareLicense']['softwareDescription']['name']
-        account['regionalGroup'] = account['regionalGroup']['name']
-        account['maxMemory'] = int(account['maxMemory'] / 1024)
-        account['powerState'] = account['powerState']['name']
-        drive_tag = 1
+    acc_conf = {}
+    df_keys = []
 
-        for disk in account['blockDevices']:
-            if 'diskImage' in disk.keys():  # assuming only relevant drives are disk drives
-                d = disk['diskImage']
-                if d['capacity'] >= 25 and d['units'] == 'GB':
-                    account['drive_' + str(drive_tag) + '_id'] = disk['id']
-                    account['drive_' + str(drive_tag) + '_capacity'] = (str(d['capacity']) + d['units'])
-                    drive_tag += 1
-        del account['blockDevices']  # drop duplicate info
-        account['monitoringAgents'] = bool(account['monitoringAgents'])
-        fields = account.keys()
-        sys_conf[account['id']] = list(account.values())
+    for system in masked_call:
+        sys_conf = {}
+        sys_conf['host_name'] = system['hostname']
+        sys_conf['HW Total CPUs'] = system['maxCpu']
+        sys_conf['HW Total Memory'] = system['maxMemory']
+        sys_conf['HW Manufacturer'] = 'IBM-Cloud'
+        sys_conf['OS Version'] = system['operatingSystem']['softwareLicense']['softwareDescription']['name']
+        if 'WINDOWS' in sys_conf['OS Version'].upper().strip():
+            sys_conf['OS Name'] = 'Windows'
+        else:
+            sys_conf['OS Name'] = 'Linux'
+        global id_host_map
+        id_host_map[system['id']] = system['hostname']
+        df_keys = sys_conf.keys()
+        acc_conf[sys_conf['host_name']] = list(sys_conf.values())
 
-    df = DataFrame.from_dict(sys_conf, orient='index', columns=fields)
-    df = df.drop('id', axis=1)
-    df.index.name = 'id'
+    df = DataFrame.from_dict(acc_conf, orient='index', columns=df_keys)
+    df = df.drop('host_name', axis=1)
+    df.index.name = 'host_name'
     return df
 
 
+def get_attributes():
+    print("getting attributes ...")
+    object_mask = "mask[hostname, id, primaryIpAddress, provisionDate, datacenter.name, regionalGroup.name, " \
+                  "powerState, domain]"
+    masked_call = client.call('Account', 'getVirtualGuests', mask=object_mask)
+    acc_attr = {}
+    for system in masked_call:
+        sys_attr = {}
+        sys_attr['host_name'] = system['hostname']
+        sys_attr['Instance ID'] = system['id']
+        sys_attr['Instance IP'] = system['primaryIpAddress']
+        sys_attr['Launch Time'] = system['provisionDate']
+        sys_attr['Virtual Cluster'] = system['datacenter']['name']
+        sys_attr['Virtual Datacenter'] = system['regionalGroup']['name']
+        sys_attr['virtual Domain'] = system['domain']
+        sys_attr['Virtual Technology'] = 'IBM-Cloud'
+        sys_attr['Power State'] = system['powerState']['name']
+        df_keys = sys_attr.keys()
+        acc_attr[sys_attr['host_name']] = list(sys_attr.values())
+
+    df = DataFrame.from_dict(acc_attr, orient='index', columns=df_keys)
+    df = df.drop('host_name', axis=1)
+    df.index.name = 'host_name'
+    return df
+
+
+def get_drives():
+    print("getting drives ...")
+    object_mask = "mask[id, blockDevices[device, id, diskImage[capacity, units]]]"
+
+    masked_call = client.call('Account', 'getVirtualGuests', mask=object_mask)
+    acc_drives = {}
+    for system in masked_call:
+        sys_drives = {}
+        drive_tag = 1
+        for disk in system['blockDevices']:
+            if 'diskImage' in disk.keys():  # assuming only relevant drives are disk drives
+                d = disk['diskImage']
+                if d['capacity'] >= 10 and d['units'] == 'GB':
+                    sys_drives['drive_' + str(drive_tag) + '_id'] = disk['id']
+                    sys_drives['drive_' + str(drive_tag) + '_capacity'] = (str(d['capacity']) + d['units'])
+                    drive_tag += 1
+        acc_drives[system['id']] = sys_drives
+
+
 def get_agents():
+    print("getting agents ...")
     sys_agents = {}
     response = client.call('Account', 'getVirtualGuests', mask='mask[monitoringAgents[id, name, configurationValues]]')
-    global calls
-    calls += 1
-    print("get_agents", calls)
     for account in response:
         if account['monitoringAgents']:
             agents = [agent for agent in account['monitoringAgents'] if agent['name'] == agentName]
@@ -101,6 +151,7 @@ def get_agents():
 
 
 def get_agent_datatypes(s_agents):
+    print("getting agent datatypes ...")
     agent_config_service = client['Monitoring_Agent_Configuration_Value']
     sys_datatypes = {}
     for vsi_id in s_agents.keys():
@@ -114,9 +165,6 @@ def get_agent_datatypes(s_agents):
                         if value['value'].strip().upper() == "TRUE":
                             try:
                                 response = agent_config_service.getMetricDataType(id=value['id'])
-                                global calls
-                                calls += 1
-                                print("get a data types", calls)
                                 metric_data_types.append(response)
                             except SoftLayer.SoftLayerAPIError as e:
                                 print("Unable to get the metrics. " % (e.faultCode, e.faultString))
@@ -132,8 +180,9 @@ def get_agent_datatypes(s_agents):
 
 
 def get_agent_metrics(s_datatypes):
+    print("getting agent metrics ...")
     acc_data = {}
-    metrics = ("cpu_utilization", "memory_utilization", "disk_space_used")
+    metrics = ("cpu_utilization", "memory_utilization", "disk_space_used")  # TODO map datatype name to human readable
 
     for vsi_id in s_datatypes.keys():
         data = {}
@@ -141,9 +190,6 @@ def get_agent_metrics(s_datatypes):
             agent_id = s_datatypes[vsi_id].pop()
             response = client.call("Monitoring_Agent", "getGraphData", s_datatypes[vsi_id], startDate, endDate,
                                    id=agent_id)
-            global calls
-            calls += 1
-            print("get a metrics", calls)
             if len(response) != 0:
                 filter_data_points(data, response)
             else:  # if no value returned from api
@@ -156,17 +202,25 @@ def get_agent_metrics(s_datatypes):
         for key, value in data.items():
             data[key] = Series(data=list(value.values()), index=value.keys())
         inst_ts = concat(data.values(), axis=1, keys=metrics, sort=False)
-        acc_data[vsi_id] = inst_ts
+        global id_host_map
+        if vsi_id in id_host_map.keys():
+            acc_data[id_host_map[vsi_id]] = inst_ts
 
     df = concat(acc_data.values(), keys=acc_data.keys(), sort=True)
-    df.index.names = ['id', "dateTime"]
+    df.index.names = ['host_name', "Datetime"]
     return df
 
 
 def get_guest_metrics(a_df):
-    vsi_ids = list(a_df.index.levels[0])
+    print("getting guest metrics ...")
+    global id_host_map
+    vsi_ids = []
+    for vsi_id in list(id_host_map.keys()):
+        if id_host_map[vsi_id] in list(a_df.index.levels[0]):
+            vsi_ids.append(vsi_id)
+
     metric_dict = {}
-    metrics = ["network_in", "network_out"]
+    metrics = ("network_in", "network_out")
 
     # Loops through each VSI using a VSI ID grabbed from the account
     for virtual_id in vsi_ids:
@@ -174,7 +228,6 @@ def get_guest_metrics(a_df):
         inst_dict = {}
         #  selects correct api call format
         response = client.call('Virtual_Guest', 'getBandwidthDataByDate', startDate, endDate, id=virtual_id)
-
         # Reformats API response and stores in inst_dict
         filter_data_points(inst_dict, response)
 
@@ -182,9 +235,10 @@ def get_guest_metrics(a_df):
         for key, value in inst_dict.items():
             inst_dict[key] = Series(data=list(value.values()), index=value.keys())
         inst_ts = concat(inst_dict.values(), axis=1, keys=metrics, sort=True)
-        metric_dict[virtual_id] = inst_ts
-    df = concat(metric_dict.values(), keys=vsi_ids, sort=True)
-    df.index.names = ['id', "dateTime"]
+        metric_dict[id_host_map[virtual_id]] = inst_ts
+
+    df = concat(metric_dict.values(), keys=metric_dict.keys(), sort=True)
+    df.index.names = ['host_name', "Datetime"]
     df = concat([df, a_df], axis=1)
     return df
 
@@ -195,13 +249,13 @@ def filter_data_points(curr_dict, response):
     for data_point in response:  # populating inst dict with type : {time : value}
         if data_point['type'] not in curr_dict.keys():  # handling of multiple metrics returned
             curr_dict[data_point['type']] = {}
-        data_point['dateTime'] = normalize_date_time(data_point['dateTime'])
+        data_point['dateTime'] = normalize_datetime(data_point['dateTime'])
         curr_dict[data_point['type']][data_point['dateTime']] = data_point['counter']
 
 
 # rounds minute down to nearest multiple of 5, and sets second and
 # millisecond values of time series to 0 to ensure proper concat
-def normalize_date_time(dt_string):
+def normalize_datetime(dt_string):
     dt_string = dt_string[::-1].replace(':', '', 1)
     dt_string = dt_string[::-1]
     d_time = datetime.strptime(dt_string, "%Y-%m-%dT%H:%M:%S%z")
